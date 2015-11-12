67a85c280d14dbc46bc6a382e57e5487b3aab01c
hengwu <youchuangqiba@163.com>
2015-11-13 01:10:09 +0800

    update: energy of audio buffer as webrtc does.

--- plugins/janus_audiobridge.c
+++ plugins/janus_audiobridge.c
@@ -393,6 +393,9 @@ record_file =	/path/to/recording.wav (where to save the recording)
 #define LCHILD(x) 2 * x + 1
 #define RCHILD(x) 2 * x + 2
 #define PARENT(x) x / 2
+/* for Energy Computing */
+#define WEBRTC_SPL_MUL(a, b) \
+    ((opus_int32) ((opus_int32)(a) * (opus_int32)(b)))
 
 /* Plugin methods */
 janus_plugin *create(void);
@@ -526,6 +529,78 @@ void janus_audiobridge_buffer_minheap_heapify(janus_audiobridge_buffer_minheap *
 }
 void janus_audiobridge_buffer_minheap_free(janus_audiobridge_buffer_minheap *hp) {
 	free(hp->elem);
+}
+opus_int16 WebRtcSpl_NormW32(opus_int32 a);
+opus_int16 WebRtcSpl_GetSizeInBits(opus_uint32 n);
+opus_int16 WebRtcSpl_GetScalingSquare(opus_int16 *in_vector, int in_vector_length, int times);
+opus_int32 janus_audiobridge_buffer_energy(opus_int16 *_buffer, int _length, int *scale_factor);
+opus_int16 WebRtcSpl_NormW32(opus_int32 a) {
+	opus_int16 zeros;
+
+	if (a == 0) {
+		return 0;
+	} else if (a < 0) {
+		a = ~a;
+	}
+
+	if (!(0xFFFF8000 & a)) {
+		zeros = 16;
+	} else {
+		zeros = 0;
+	}
+	if (!(0xFF800000 & (a << zeros))) zeros += 8;
+	if (!(0xF8000000 & (a << zeros))) zeros += 4;
+	if (!(0xE0000000 & (a << zeros))) zeros += 2;
+	if (!(0xC0000000 & (a << zeros))) zeros += 1;
+
+	return zeros;
+}
+opus_int16 WebRtcSpl_GetSizeInBits(opus_uint32 n) {
+	opus_int16 bits;
+
+	if (0xFFFF0000 & n) {
+		bits = 16;
+	} else {
+		bits = 0;
+	}
+	if (0x0000FF00 & (n >> bits)) bits += 8;
+	if (0x000000F0 & (n >> bits)) bits += 4;
+	if (0x0000000C & (n >> bits)) bits += 2;
+	if (0x00000002 & (n >> bits)) bits += 1;
+	if (0x00000001 & (n >> bits)) bits += 1;
+
+	return bits;
+}
+opus_int16 WebRtcSpl_GetScalingSquare(opus_int16 *in_vector, int in_vector_length, int times) {
+	opus_int16 nbits = WebRtcSpl_GetSizeInBits((opus_uint32)times);
+    int i;
+    opus_int16 smax = -1;
+    opus_int16 sabs;
+    opus_int16 *sptr = in_vector;
+    opus_int16 t;
+    int looptimes = in_vector_length;
+
+    for (i = looptimes; i > 0; i--) {
+        sabs = (*sptr > 0 ? *sptr++ : -*sptr++);
+        smax = (sabs > smax ? sabs : smax);
+    }
+    t = WebRtcSpl_NormW32(WEBRTC_SPL_MUL(smax, smax));
+
+    if (smax == 0) {
+        return 0; // Since norm(0) returns 0
+    } else {
+        return (t > nbits) ? 0 : nbits - t;
+    }
+}
+opus_int32 janus_audiobridge_buffer_energy(opus_int16 *_buffer, int _length, int *scale_factor) {
+    opus_int32 energy = 0;
+    *scale_factor = WebRtcSpl_GetScalingSquare(_buffer, _length, _length);
+
+    for (int i = 0, opus_int16 *p = _buffer; i < _length; i++) {
+        energy += (*p * *p) >> (*scale_factor);
+        p++;
+    }
+    return energy;
 }
 //-->
 
@@ -2718,7 +2793,7 @@ static void *janus_audiobridge_mixer_thread(void *data) {
 
 		//<!-- find the top k strongest buffers
 		int participantCounter = 0, participantNumber = g_list_length(participants_list);
-		int topKofAll = participantNumber, _topKofAllInit = 3; // set k
+		int topKofAll = participantNumber, _topKofAllInit = 2; // set k
 		if(topKofAll > _topKofAllInit) topKofAll = _topKofAllInit;
 		janus_audiobridge_buffer_minheap heapofBuffers; // a min-heap
 		janus_audiobridge_buffer_minheap_init(&heapofBuffers, topKofAll);
@@ -2739,9 +2814,13 @@ static void *janus_audiobridge_mixer_thread(void *data) {
 				curBuffer = (opus_int16 *)pkt->data;
 				// calculate its strength value:
 				opus_int32 _curBufferStrength = 0;
+				/*
 				for(i = 0; i < samples; i++) {
 					_curBufferStrength += curBuffer[i];
 				}
+				*/
+				int scale_factor = 0;
+				_curBufferStrength = janus_audiobridge_buffer_energy(curBuffer, samples, &scale_factor);
 				// now we get (participantCounter, _curBufferStrength, curBuffer).
 				janus_audiobridge_buffer_minheap_insert(&heapofBuffers, participantCounter, _curBufferStrength, curBuffer);
 			}
